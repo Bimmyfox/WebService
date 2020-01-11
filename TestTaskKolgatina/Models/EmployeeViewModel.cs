df0cd8a48ba578a3baf5ae3698e39b887c5a852b
Bimmyfox <kolgatinadasha@gmail.com>
2020-01-11 03:40:23 +0300

    added a draft employee's model

---
+++ TestTaskKolgatina/Models/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTaskKolgatina.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public int CompanyId { get; set; }
+        public string Passport { get; set; }
+        // Passport { Type string Number string }
+    }
+}
